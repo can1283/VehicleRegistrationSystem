d69fce72a87972204ac4d0346552cb2eaffb8489
Nurican Kaşıkcı <nuricankasikci@gmail.com>
2023-10-05 13:58:34 +0300

    Initial commit

---
+++ src/main/java/com/vehicleregistrationsystem/finalcase/VehicleRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vehicleregistrationsystem.finalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRegistrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vehicleregistrationsystem/finalcase/VehicleRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleregistrationsystem.finalcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
