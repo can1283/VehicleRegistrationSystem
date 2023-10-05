be13e056e251239066856b374e400272fa1bde7c
Nurican Kaşıkcı <nuricankasikci@gmail.com>
2023-10-05 15:47:58 +0300

    add entity class

--- src/main/java/com/vehicleregistrationsystem/finalcase/model/entity/User.java
+++ src/main/java/com/vehicleregistrationsystem/finalcase/model/entity/User.java
@@ -1,15 +1,45 @@
 package com.vehicleregistrationsystem.finalcase.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.vehicleregistrationsystem.finalcase.model.enums.Cities;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String userName;
     private String password;
     private String firstName;
     private String lastName;
+
+    @Enumerated(EnumType.STRING)
     private Cities city;
+
+    @Temporal(value = TemporalType.DATE)
     private Date accountCreationDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonManagedReference
+    private List<Vehicle> vehicles;
+
+    public User(String userName, String password, String firstName, String lastName, Cities city, Date accountCreationDate, List<Vehicle> vehicles) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.accountCreationDate = accountCreationDate;
+        this.vehicles = vehicles;
+    }
 }
--- src/main/java/com/vehicleregistrationsystem/finalcase/model/entity/Vehicle.java
+++ src/main/java/com/vehicleregistrationsystem/finalcase/model/entity/Vehicle.java
@@ -1,10 +1,37 @@
 package com.vehicleregistrationsystem.finalcase.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "vehicles")
 public class Vehicle {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
     private String brand;
     private String model;
     private String plateCode;
     private String modelYear;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JsonBackReference
     private User user;
+
+    public Vehicle(String name, String brand, String model, String plateCode, String modelYear, User user) {
+        this.name = name;
+        this.brand = brand;
+        this.model = model;
+        this.plateCode = plateCode;
+        this.modelYear = modelYear;
+        this.user = user;
+    }
 }
